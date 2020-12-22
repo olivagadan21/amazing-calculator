4fa68264537f31a09482590dafeef0335b18cc2e
Daniel Oliva <oliva.gadan21@triana.salesianos.edu>
2020-12-22 09:23:26 +0100

    Primer commit del proyecto

---
+++ Amazing-calculator/src/calculator/IncredibleCalculator.java
@@ -0,0 +1,71 @@
+package calculator;
+
+public class IncredibleCalculator {
+	public static void main(String[] args) {
+
+        System.out.println("Selecciona la operación que prefieras realizar: ");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir (división entera)");
+        System.out.println("5. Dividir (división con decimales)");
+
+        int opcion = leerEnteroEntrada();
+
+
+        switch (opcion) {
+            case 1: 
+            	//Aquí debería ir el código que realizaría la suma de los números escaneados.
+                    break;
+            case 2: 
+            	//Aquí debería ir el código que realizaría la resta de los números escaneados.
+                    break;
+            case 3:
+            	//Aquí debería ir el código que realizaría la multiplicación de los números escaneados.
+                    break;
+            case 4: 
+            	//Aquí debería ir el código que realizaría la división con números enteros de los números escaneados.
+                    break;
+            case 5: 
+            	//Aquí debería ir el código que realizaría la división con números decimales de los números escaneados.
+                    break;
+
+        }
+
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static int resta(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public static int divisionEntera(int a, int b) {
+        return a / b;
+    }
+
+    public static double divisionConDecimales(int a, int b) {
+        return a / (b * 1.0);
+        
+    }
+
+    public static int leerEnteroEntrada() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int result = Integer.parseInt(sc.next());
+            sc.close();
+            return result;
+        } catch (NumberFormatException ex) {
+            System.err.println("Lo que has introducido no es un número entero!");
+            System.exit(1);
+            return -1;
+        }
+
+    }
+}
